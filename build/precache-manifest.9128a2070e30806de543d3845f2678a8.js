8152aae9bf944da54962f6d4294858d5cbb7eb78
plinck <paul.linck@gmail.com>
2019-04-02 10:21:16 -0400

    Initial push

---
+++ build/precache-manifest.9128a2070e30806de543d3845f2678a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bd3b02f3b010b1a0614b",
+    "url": "/static/css/main.91904de7.chunk.css"
+  },
+  {
+    "revision": "bd3b02f3b010b1a0614b",
+    "url": "/static/js/main.82bd7657.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "eca5f8b01333ea02eba2",
+    "url": "/static/js/2.12e23674.chunk.js"
+  },
+  {
+    "revision": "3e449ab5cb2729defc3f71ad350832f3",
+    "url": "/index.html"
+  }
+];
